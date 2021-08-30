d416db925e7dafc4029829d7e02cb4a9e27c028b
kumarkaushal995@gmail.com <kumarkaushal995@gmail.com>
2021-08-31 00:49:48 +0530

    implemented sort for bowlers birthday

--- WebScrapping/Bowlers_Data/birthday.js
+++ WebScrapping/Bowlers_Data/birthday.js
@@ -3,6 +3,8 @@ let fs = require("fs");
 let request = require("request");
 let cheerio = require("cheerio");
 let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/royal-challengers-bangalore-vs-sunrisers-hyderabad-eliminator-1237178/full-scorecard";
+let bowlersArr = [];
+let bowlersCount = 0;
 request(url,cb);
 function cb(error,response,html){
     // console.error('error', error)
@@ -38,7 +40,13 @@ function dataExtractor(html){
     //     htmlData += searchTool(bowlerTables[i]).html();
     // }
     // fs.writeFileSync("table.html", htmlData);
+    for(let i = 0; i < bowlers.length; i++){
+        let cols = searchTool(bowlers[i]).find("td");
+        if(cols.length > 1){
+            bowlersCount++;
 
+        }
+    }
 
     // for data of bowlers 
     // looop
@@ -46,12 +54,15 @@ function dataExtractor(html){
     for(let i = 0; i < bowlers.length; i++){
         // row - > col means cols in row 
         let cols = searchTool(bowlers[i]).find("td");
-        let aElem = searchTool(cols[0]).find("a");
-        let link = aElem.attr("href");
-        //link 
-        // new page -> link get -> complete -> request
-        let fullLink = `https://www.espncricinfo.com/${link}`;
-        request(fullLink, newcb);
+        if(cols.length > 1){
+            let aElem = searchTool(cols[0]).find("a");
+            let link = aElem.attr("href");
+            //link 
+            // new page -> link get -> complete -> request
+            let fullLink = `https://www.espncricinfo.com/${link}`;
+            request(fullLink, newcb);
+        }
+
     }
 }
 
@@ -64,8 +75,14 @@ function newcb(error, response, html){
     else{
         // console.log(html);
         // console.log("html",)
-        console.log("`````````````````````````````````````````")
+        // console.log("`````````````````````````````````````````")
         getBirthDay(html);
+        if(bowlersArr.length == bowlersCount){
+            console.table(bowlersArr);
+            let sortedArray = sortArray(bowlersArr);
+            console.table(sortedArray);
+        }
+        
     }
 }
 function getBirthDay(html){
@@ -73,8 +90,21 @@ function getBirthDay(html){
     let elemRepresentativeArray = searchTool(".player-card-description");
     let name = searchTool(elemRepresentativeArray[0]).text();
     let age = searchTool(elemRepresentativeArray[2]).text();
-    console.log(name +" "+age);
-    
+    bowlersArr.push({name, age});
+    // console.log(name +" "+age);
+}
+function sortArray(bowlersArr){
+    bowlersArray = bowlersArr.sort(comparator);
+    return bowlersArr;
+}
+function comparator(objA,objB){
+       let ageArrayA = objA.age.split(" "); 
+       let ageArrayB = objB.age.split(" ");
+       let yearsofAgeA = Number(ageArrayA[0].substring(0, ageArrayA[0].length - 1));
+       let yearsofAgeB = Number(ageArrayB[0].substring(0, ageArrayB[0].length - 1));
+       let numDaysA = Number(ageArrayA[1].substring(0, ageArrayA[1].length - 1));
+       let numDaysB = Number(ageArrayB[1].substring(0, ageArrayB[1].length - 1));
+       return yearsofAgeA - yearsofAgeB || numDaysA - numDaysB;
 
 
 }
