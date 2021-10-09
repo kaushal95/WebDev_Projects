3a304aea50e748a5923548eba2bf0bec5078b371
kumarkaushal995@gmail.com <kumarkaushal995@gmail.com>
2021-10-09 15:40:41 +0530

    local Storage functionality

--- Jira_Ticket_Management/script.js
+++ Jira_Ticket_Management/script.js
@@ -1,69 +1,164 @@
 var uid = new ShortUniqueId();
 // console.log(uid());
 
-// let colCode = document.querySelectorAll(".color-filter");
+//variables used 
+let defaultColor = "black";
+let cFilter = "";
+let deleteActive = false;
+let colors = ["pink", "blue", "green", "black"];
+
+//elements 
 let mainContainer = document.querySelector(".main-container");
 let textInput = document.querySelector(".task_input");
 let addDel = document.querySelector(".add-delete");
-let colors = ["pink", "blue", "green", "black"];
-let defaultColor = "black";
-let cFilter = "";
-// colCode.addEventListener("click",function(event){
-//     console.log("clicked");
-// })
-// addDel.addEventListener("click",function(event){
-    // let target = event.target;
-let i = 0;
-// let colChange;
-textInput.addEventListener("keydown",function(event){
+let lock = document.querySelector(".lock");
+let unlock = document.querySelector(".unlock");
+let deleteButton = document.querySelector(".delete");
+let colCode = document.querySelector(".color-filter");
 
+
+// Event Listeners
+
+//
+textInput.addEventListener("keydown",function(event){
     if(event.code == "Enter" && textInput.value){
-        i++;
         let tid = uid();
-        createModal(tid, textInput.value);
+        createModal(tid, textInput.value, true);
         textInput.value = "";
     }    
-    // console.log(target.getattribute("id"));
-    // console.log("et  ",event.target);
-    // console.log("eCt  ",event.currentTarget);
-    
 })
-console.log(i);
-function createModal(tid, value){
-    let tBox = document.createElement("div");
-    tBox.className = "text-box";
-    tBox.innerHTML = `<div class="text-header 
-    ${defaultColor}">
-    
-    </div>
-    <div class="text-container">
-    <h3 class="text-id">${tid}</h3>    
-    <div class="text-content">${value}</div>
-    </div>`;
-    mainContainer.appendChild(tBox)
-    let colChange = tBox.querySelector(".text-header");
-    colChange.addEventListener("click",function(event){
-        console.log(colChange.classList);
-        let cColor = colChange.classList[1];
-        let idx = colors.indexOf(cColor);
-        let nxtidx = (idx + 1) % 4; 
-        colChange.classList.remove(cColor);
-        colChange.classList.add(colors[nxtidx]);
-        // console.log(currentCurrentClass);
-    })
-}
 
 
 // filtering functinality based on particular color 
-let colCode = document.querySelector(".color-filter");
 colCode.addEventListener("click", function(event){
-    console.log(event.currentTarget);
-    console.log("ettttttttt   ", event.target)
     let filterColor = event.target.classList[1];
     console.log(filterColor);
     filterCard(filterColor);
 })
 
+//delete button functionality active/shadow function
+deleteButton.addEventListener("click",function(){
+    deleteActive = !deleteActive;
+    if(deleteActive == true){
+        deleteButton.classList.add("active");
+    }else{
+        deleteButton.classList.remove("active");
+    }
+})
+
+//lock button functionality active/shadow function
+lock.addEventListener("click",function(){
+    let textContainers = document.querySelectorAll(".text-content");
+
+    for (let i = 0; i < textContainers.length; i++){
+        console.log("hi");
+        textContainers[i].contentEditable = "false";
+    }
+    lock.classList.add("active");
+    unlock.classList.remove("active");
+})
+
+// unlock button functionality active/shadow function
+unlock.addEventListener("click",function(){
+    let textContainers = document.querySelectorAll(".text-content");
+    for (let i = 0; i < textContainers.length; i++){
+        textContainers[i].contentEditable = "true";
+    }
+    unlock.classList.add("active");
+    lock.classList.remove("active");
+})
+
+// helper functions 
+
+//create to-do task bar 
+function createModal(tid, value,flag,color){
+    // to create new task bar
+    let tBox = document.createElement("div");
+    tBox.className = "text-box";
+    tBox.innerHTML = `<div class="text-header 
+    ${color ? color : defaultColor}">
+    
+    </div>
+    <div class="text-container">
+    <h3 class="text-id">#${tid}</h3>    
+    <div class="text-content" contenteditable="true">${value}</div>
+    </div>`;
+    mainContainer.appendChild(tBox)
+    let colChange = tBox.querySelector(".text-header");
+    let nextcolor;
+
+    //filter change in ui and filter update function in localstorage
+    colChange.addEventListener("click",function(event){
+        // console.log(colChange.classList);
+        let cColor = colChange.classList[1];
+        let idx = colors.indexOf(cColor);
+        let nxtidx = (idx + 1) % 4; 
+        nextcolor = colors[nxtidx];
+        colChange.classList.remove(cColor);
+        colChange.classList.add(nextcolor);
+
+        let tasksString = localStorage.getItem("tasks");
+        let tasksArr = JSON.parse(tasksString) || [];
+        let taskid = colChange.parentNode.children[1].children[0].textContent.substring(1)
+        for(let i = 0; i < tasksArr.length; i++){
+            // console.log("tasks :  ", tasksArr[i]);
+            if(tasksArr[i].tid == taskid){
+                // console.log("tid     : ", tasksArr[i].tid, tasksArr[i].color, nextcolor);
+                tasksArr[i].color = nextcolor;
+            }
+        }
+        localStorage.setItem("tasks", JSON.stringify(tasksArr));
+    })
+
+    // delete functionality ui & localstorage 
+    tBox.addEventListener("click",function(){
+        console.log(tid);
+        if(deleteActive == true){
+            let tasksArr = JSON.parse(localStorage.getItem("tasks")) || [];
+            console.log(tasksArr);
+            
+            for(let i = 0; i < tasksArr.length; i++){
+                // console.log("inner tid ", tid, " tasks tid ", tasksArr[i].tid);
+
+                if(tid == tasksArr[i].tid){
+                    // console.log("inner tid ", tid, " tasks tid ", tasksArr[i].tid);
+                    tasksArr.splice(i,1);
+                    break;
+                }
+    
+            }
+            localStorage.setItem("tasks",JSON.stringify(tasksArr));
+            tBox.remove();
+        }
+    })
+    let inputContainer = document.querySelector(".text-content");
+    inputContainer.addEventListener("blur",function(e){
+        let content = inputContainer.textContent;
+        let tasksArr = JSON.parse(localStorage.getItem("tasks"));
+        for(let i = 0; i < tasksArr.length; i++){
+            if(tid == tasksArr[i].tid){
+                tasksArr[i].value = content;
+                break;
+            }
+        }
+        localStorage.setItem("tasks",JSON.stringify(tasksArr));
+    })
+
+    //appending new elements created using ui to local storage 
+    if (flag == true) {
+        let tasksString = localStorage.getItem("tasks");
+        let tasksArr = JSON.parse(tasksString) || [];
+        let taskObject = {
+            tid: tid,
+            value: value,
+            color: nextcolor
+        }
+        tasksArr.push(taskObject);
+        localStorage.setItem("tasks", JSON.stringify(tasksArr));
+    }
+}
+
+//Filtering based on color
 function filterCard(filterColor){
     let allElems = mainContainer.querySelectorAll(".text-header");
     if(cFilter != filterColor){
@@ -84,5 +179,15 @@ function filterCard(filterColor){
 
 }
 
+//localStorage function 
+(function () {
+    // localStorage
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    for (let i = 0; i < tasks.length; i++) {
+        let { tid, value, color } = tasks[i];
+        createModal(tid,value,false,color);
+    }
+    // get it to ui
+})();
 
 
